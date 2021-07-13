da2572e7d3b92f698e4c5d58162fceb12ccb1aab
Ash-31win <71268108+Ash-31win@users.noreply.github.com>
2021-07-13 23:47:20 +0530

    Add files via upload

---
+++ bag_of_words3.py
@@ -0,0 +1,281 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 24 15:04:44 2021
+
+@author: HP
+"""
+# General packages
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import os
+
+# NLP packages
+import nltk
+from nltk import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter
+from wordcloud import WordCloud
+
+# Modeling packages
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+
+from pylab import rcParams
+import warnings
+warnings.filterwarnings("ignore")
+rcParams['figure.figsize'] = 14, 6
+plt.style.use('ggplot')
+
+
+amazon_reviews = pd.read_csv('https://media.githubusercontent.com/media/juliandariomirandacalle/NLP_Notebooks/master/01-Introduction_NLP/Customer_Reviews.csv')
+amazon_reviews.head(3)
+
+
+## Getting the number of words by splitting them by a space
+words_per_review = amazon_reviews.Text.apply(lambda x: len(x.split(" ")))
+words_per_review.hist(bins = 100)
+plt.xlabel('Review Length (words)')
+plt.ylabel('Frequency')
+plt.show()
+
+print('Average words:', words_per_review.mean())
+print('Skewness:', words_per_review.skew())
+
+percent_val = 100 * amazon_reviews['Score'].value_counts()/len(amazon_reviews)
+percent_val
+
+percent_val.plot.bar()
+plt.show()
+
+word_cloud_text = ''.join(amazon_reviews['Text'])
+
+wordcloud = WordCloud(max_font_size=100, # Maximum font size for the largest word
+                      max_words=100, # The maximum number of words
+                      background_color="white", # Background color for the word cloud image
+                      scale = 10, # Scaling between computation and drawing
+                      width=800, # Width of the canvas
+                      height=400 # Height of the canvas
+                     ).generate(word_cloud_text)
+
+plt.figure()
+plt.imshow(wordcloud, 
+           interpolation="bilinear") # to make the displayed image appear more smoothly
+plt.axis("off")
+plt.show()
+
+
+# Mapping the ratings
+amazon_reviews['Sentiment_rating'] = np.where(amazon_reviews.Score > 3,1,0)
+
+## Removing neutral reviews 
+amazon_reviews = amazon_reviews[amazon_reviews.Score != 3]
+
+# Printing the counts of each class
+amazon_reviews['Sentiment_rating'].value_counts()
+
+
+amazon_reviews.Sentiment_rating.value_counts().plot.bar()
+plt.show()
+
+amazon_reviews['reviews_text_new'] = amazon_reviews['Text'].str.lower()
+
+
+from nltk import word_tokenize
+
+
+
+import nltk
+nltk.download('punkt')
+
+# For reviews not converted to lowe case
+token_lists = [word_tokenize(each) for each in amazon_reviews['Text']]
+tokens = [item for sublist in token_lists for item in sublist]
+print("Number of unique tokens then: ",len(set(tokens)))
+
+# For reviews converted to lowe case
+token_lists_lower = [word_tokenize(each) for each in amazon_reviews['reviews_text_new']]
+tokens_lower = [item for sublist in token_lists_lower for item in sublist]
+print("Number of unique tokens now: ",len(set(tokens_lower)))
+
+### Selecting non alpha numeric charactes that are not spaces
+spl_chars = amazon_reviews['reviews_text_new'].apply(lambda review: 
+                                                     [char for char in list(review) if not char.isalnum() and char != ' '])
+
+## Getting list of list into a single list
+flat_list = [item for sublist in spl_chars for item in sublist]
+
+## Unique special characters
+set(flat_list)
+
+review_backup = amazon_reviews['reviews_text_new'].copy()
+amazon_reviews['reviews_text_new'] = amazon_reviews['reviews_text_new'].str.replace(r'[^A-Za-z0-9 ]+', ' ')
+
+print("- Old Review -")
+print(review_backup.values[6])
+print("\n- New Review -")
+print(amazon_reviews['reviews_text_new'][6])
+
+
+token_lists = [word_tokenize(each) for each in amazon_reviews['Text']]
+tokens = [item for sublist in token_lists for item in sublist]
+print("Number of unique tokens then: ",len(set(tokens)))
+
+token_lists = [word_tokenize(each) for each in amazon_reviews['reviews_text_new']]
+tokens = [item for sublist in token_lists for item in sublist]
+print("Number of unique tokens now: ",len(set(tokens)))
+
+nltk.download('stopwords')
+
+
+from nltk.corpus import stopwords
+
+print('Available languages for NLTK v.3.4.5: ')
+print(stopwords.fileids())
+
+noise_words = []
+eng_stop_words = stopwords.words('english')
+eng_stop_words
+
+stop_words = set(eng_stop_words)
+without_stop_words = []
+stopword = []
+sentence = amazon_reviews['reviews_text_new'][0]
+words = nltk.word_tokenize(sentence)
+
+for word in words:
+    if word in stop_words:
+        stopword.append(word)
+    else:
+        without_stop_words.append(word)
+
+print('-- Original Sentence --\n', sentence)
+print('\n-- Stopwords in the sentence --\n', stopword)
+print('\n-- Non-stopwords in the sentence --\n', without_stop_words)
+
+def stopwords_removal(stop_words, sentence):
+    return [word for word in nltk.word_tokenize(sentence) if word not in stop_words]
+
+amazon_reviews['reviews_text_nonstop'] = amazon_reviews['reviews_text_new'].apply(lambda row: stopwords_removal(stop_words, row))
+amazon_reviews[['reviews_text_new','reviews_text_nonstop']]
+
+from nltk.stem import PorterStemmer, LancasterStemmer # Common stemmers
+from nltk.stem import WordNetLemmatizer # Common Lematizer
+nltk.download('wordnet')
+from nltk.corpus import wordnet
+
+porter = PorterStemmer()
+lancaster = LancasterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+print("Lancaster Stemmer")
+print(lancaster.stem("trouble"))
+print(lancaster.stem("troubling"))
+print(lancaster.stem("troubled"))
+
+# Provide a word to be lemmatized
+print("WordNet Lemmatizer")
+print(lemmatizer.lemmatize("trouble", wordnet.NOUN))
+print(lemmatizer.lemmatize("troubling", wordnet.VERB))
+print(lemmatizer.lemmatize("troubled", wordnet.VERB))
+
+amazon_reviews[['Text','Score','Sentiment_rating']].head(5)
+
+
+from nltk import ngrams
+
+sentence = 'A bird in the hand worths two in the bush'
+
+for n in range(1, 6):
+    print(str(n) + '-grams:\n', list(ngrams(sentence.split(), n)))
+
+# The following code creates a word-document matrix.
+from sklearn.feature_extraction.text import CountVectorizer
+
+vec = CountVectorizer()
+X = vec.fit_transform(amazon_reviews['reviews_text_new'])
+df = pd.DataFrame(X.toarray(), columns = vec.get_feature_names())
+df.head()
+
+## Creating a python object of the class CountVectorizer
+
+bow_counts = CountVectorizer(tokenizer= word_tokenize, # type of tokenization
+                             stop_words=noise_words, # List of stopwords
+                             ngram_range=(1,1)) # number of n-grams
+
+bow_data = bow_counts.fit_transform(amazon_reviews['reviews_text_new'])
+
+bow_data
+
+
+X_train_bow, X_test_bow, y_train_bow, y_test_bow = train_test_split(bow_data, # Features
+                                                                    amazon_reviews['Sentiment_rating'], # Target variable
+                                                                    test_size = 0.2, # 20% test size
+                                                                    random_state = 0) # random state for replication purposes
+
+y_test_bow.value_counts()/y_test_bow.shape[0]
+
+
+### Training the model 
+lr_model_all = LogisticRegression() # Logistic regression
+lr_model_all.fit(X_train_bow, y_train_bow) # Fitting a logistic regression model
+
+## Predicting the output
+test_pred_lr_all = lr_model_all.predict(X_test_bow) # Class prediction
+
+## Calculate key performance metrics
+print("F1 score: ", f1_score(y_test_bow, test_pred_lr_all))
+
+
+
+bow_counts = CountVectorizer(tokenizer= word_tokenize,
+                             ngram_range=(1,4))
+
+bow_data = bow_counts.fit_transform(amazon_reviews.reviews_text_new)
+
+# Notice the increase in features with inclusion of n-grams
+bow_data
+
+X_train_bow, X_test_bow, y_train_bow, y_test_bow = train_test_split(bow_data,
+                                                                    amazon_reviews['Sentiment_rating'],
+                                                                    test_size = 0.2,
+                                                                    random_state = 0)
+
+
+# Defining and training the model
+lr_model_all_new = LogisticRegression(max_iter = 200)
+lr_model_all_new.fit(X_train_bow, y_train_bow)
+
+# Predicting the results
+test_pred_lr_all = lr_model_all_new.predict(X_test_bow)
+
+print("F1 score: ", f1_score(y_test_bow,test_pred_lr_all))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(amazon_reviews['Text'], # Features
+                                                                    amazon_reviews['Sentiment_rating'], # Target variable
+                                                                    test_size = 0.2, # 20% test size
+                                                                    random_state = 0) # random state for replication purposes
+df_result =pd.DataFrame({'Review': X_test, 'Actual': y_test, 'predicted':test_pred_lr_all})
+df_positive = df_result[df_result['predicted'] == 1]
+df_negative = df_result[df_result['predicted'] == 0]
+
+df_positive.to_csv (r'C:\Users\HP\Desktop\sentimental analysis\df_bow4_positive.csv', index = False, header=True)    
+df_negative.to_csv (r'C:\Users\HP\Desktop\sentimental analysis\df_bow4_negative.csv', index = False, header=True)    
